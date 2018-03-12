bb08fafa266b7082e50b51494a580f10ee24ce54
PRINCESS.CELL <dragon@lalaines-mbp>
2018-03-11 20:52:09 -0700

    Todo list complete with datasource and delegate methods

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -36,6 +36,8 @@ class TodoListViewController: UITableViewController {
     
     //MARK - TableView Delegate Methods
     
+    
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print (itemArray[indexPath.row])
         
