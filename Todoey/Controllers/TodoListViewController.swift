3157c8265840727a0e31783debe0907ab845874f
PRINCESS.CELL <dragon@lalaines-mbp>
2018-03-17 06:15:11 -0700

    Persisting data using Codable protocol and Plist

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 
+
 class TodoListViewController: UITableViewController {
 
   
-    var itemArray = ["Dylan Library", "Look for a job", "Oopen house"]
+    var itemArray = [Item]()
+    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in:.userDomainMask).first?.appendingPathComponent("Items.plist")
 
     let defaults = UserDefaults.standard
     
@@ -19,10 +22,27 @@ class TodoListViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
   
-        if let items =  defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
-        }
+     
+ 
+        
+        
+        print(dataFilePath)
+        
+//        let newItem = Item()
+//        newItem.title = "Dylan Library"
+//        itemArray.append(newItem)
+//
+//        let newItem2 = Item()
+//        newItem2.title = "Look for a job"
+//        itemArray.append(newItem2)
+//
+//        let newItem3 = Item()
+//        newItem3.title = "Open House"
+//        itemArray.append(newItem3)
+    
+        loadItems()
     }
+    
 
     
     //Mark - Tableview Datasource Methods
@@ -35,9 +55,16 @@ class TodoListViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
-        return cell
+        let item = itemArray[indexPath.row]
         
+        cell.textLabel?.text = item.title
+   
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+     
+        
+             return cell
         }
     
     //MARK - TableView Delegate Methods
@@ -47,13 +74,18 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print (itemArray[indexPath.row])
         
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        saveItems()
+        
         
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
         } else {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
-        
+    
+       
         tableView.deselectRow(at: indexPath, animated: true)
         
 }
@@ -72,9 +104,12 @@ class TodoListViewController: UITableViewController {
     let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
         //what will happen once the user clicks the Add Item button on our UIAlert
         
-        self.itemArray.append(textField.text!)
-        self.defaults.set(self.itemArray, forKey: "TodoListArray")
-        self.tableView.reloadData()
+       let newItem = Item()
+      newItem.title = textField.text!
+        
+        self.itemArray.append(newItem)
+        self.saveItems()
+   
     }
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new Item"
@@ -86,4 +121,44 @@ class TodoListViewController: UITableViewController {
 }
 
 
+
+
+
+//MARK : Manipulation Methods
+
+func saveItems() {
+    
+    let encoder = PropertyListEncoder()
+    
+    do {
+        let data = try encoder.encode(itemArray)
+        try data.write(to:dataFilePath!)
+    } catch {
+        print("Error encoding item array, \(error)")
+    }
+    
+    
+    
+    tableView.reloadData()
+    
+    
+    
+}
+    
+    func loadItems() {
+      if let data = try? Data(contentsOf: dataFilePath!) {
+        let decoder = PropertyListDecoder()
+        do {
+        itemArray = try decoder.decode([Item].self, from : data)
+            } catch {
+                print("Error decoding item array, \(error)")
+            }
+    
+        }
+    }
+    
+    
+    
+    
+    
 }
